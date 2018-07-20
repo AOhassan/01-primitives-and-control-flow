4c539691f892196af7d1789c034cba771d6002ff
Ahmed Osman <ahmedhosman41@gmail.com>
2018-07-20 16:15:07 -0700

    fixed run

--- src/Pluralize.java
+++ src/Pluralize.java
@@ -1,15 +1,18 @@
 public class Pluralize {
+    public static void main(String[] args) {
+        pluralize("dog",3);
+        pluralize("dog", 1);
+    }
     public static void pluralize(String animal, int count) {
 
         String plural;
 
         if (count != 1) {
-            plural = ".";
+            plural = "s";
         }
         else {
-            plural = "'s";
-
-            System.out.println("I own " + count + " " + animal + plural );
+            plural = ".";
         }
+        System.out.println("I own " + count + " " + animal + plural );
     }
 }
